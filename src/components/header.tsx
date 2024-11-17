281b8deddac69e54030586b0018df825e0311c3a
Serhii Hurshal <seergiygurshall@gmail.com>
2024-11-17 20:13:50 +0200

    feat: initial UI for header

---
+++ src/components/header.tsx
@@ -0,0 +1,15 @@
+import { LanguagePicker } from './language-picker'
+import { Button } from './ui/button'
+
+export default function Header() {
+  return (
+    <div className='flex justify-between p-3'>
+      <p className='px-4 py-2'>LOGO</p>
+      <div className='flex gap-2'>
+        <LanguagePicker/>
+        <Button variant="outline">Sign in</Button>
+        <Button>Sign up</Button>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/language-picker.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { useState } from 'react'
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+export function LanguagePicker() {
+  const [language, setLanguage] = useState("EN")
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline">{language}</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuSeparator />
+        <DropdownMenuRadioGroup value={language} onValueChange={setLanguage}>
+          <DropdownMenuRadioItem value="EN">EN</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="DE">DE</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="UA">UA</DropdownMenuRadioItem>
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
