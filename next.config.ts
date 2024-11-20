07c84dbb6f19775076444b3bd8ab8f9213ff551c
Serhii Hurshal <seergiygurshall@gmail.com>
2024-11-20 18:13:33 +0200

    feat: added service forcer

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import withSerwistInit from "@serwist/next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const withSerwist = withSerwistInit({
+  swSrc: "./src/app/sw.ts",
+  swDest: "public/sw.js",
+});
+
+const nextConfig: NextConfig = withSerwist({});
 
 export default nextConfig;
